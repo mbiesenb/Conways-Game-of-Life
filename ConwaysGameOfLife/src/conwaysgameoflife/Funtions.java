a5696acb0eab7bf4d9a396a489c13977e817b0da
marvin.biesenbach@gmx.de <marvin.biesenbach@gmx.de>
2017-06-30 20:53:46 +0200

    Create and implement new interface for algorithm´s functions

---
+++ ConwaysGameOfLife/src/conwaysgameoflife/Funtions.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conwaysgameoflife;
+
+/**
+ *
+ * @author Marvin
+ */
+public interface Funtions {
+    void getLeft(int index);
+    void getRight(int index);
+    void getUpper(int index);
+    void getUnder(int index);
+}
--- ConwaysGameOfLife/src/conwaysgameoflife/Manager.java
+++ ConwaysGameOfLife/src/conwaysgameoflife/Manager.java
@@ -4,11 +4,40 @@
  * and open the template in the editor.
  */
 package conwaysgameoflife;
-
+import LL.LL;
 /**
  *
  * @author Marvin
  */
-public class Manager {
+public class Manager implements Funtions{
+    private final LL <Cell> cellMatrix;
+
+    public LL<Cell> getCellMatrix() {
+        return cellMatrix;
+    }
+    public Manager() {
+        this.cellMatrix = new LL<>();
+        
+    }
+
+    @Override
+    public void getLeft(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getRight(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getUpper(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getUnder(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
